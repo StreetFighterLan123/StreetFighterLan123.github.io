eabe68dc4a1af9673527e3f68f21a6fcdd088656
Omar Ansari <StreetFighterLan123@gmail.com>
2020-11-04 09:37:34 -0600

    Added the typing test code.

---
+++ main.js
@@ -0,0 +1,78 @@
+let quote = "about above add after again air all almost along also always America an and animal another answer any are around as ask at away back be because been before began begin being below between big book both boy but by call came can car carry change children city close come could country cut day did different do does don't down each earth eat end enough even every example eye face family far father feet few find first follow food for form found four from get girl give go good got great group grow had hand hard has have he head hear help her here high him his home house how idea if important in Indian into is it its it's just keep kind know land large last later learn leave left let letter life light like line list little live long look made make man many may me mean men might mile miss more most mother mountain move much must my name near need never new next night no not now number of off often oil old on once one only open or other our out over own page paper part people picture place plant play point put question quick quickly quite read really right river run said same saw say school sea second see seem sentence set she should show side small so some something sometimes song soon sound spell start state still stop story study such take talk tell than that the their them then there these they thing think this those thought three through time to together too took tree try turn two under until up us use very walk want was watch water way we well went were what when where which while white who why will with without word work world would write year you young your above add after again air all almost along also always America an and animal another answer any are around as ask at away back be because been before began begin being below between big book both boy but by call came can car carry change children city close come could country cut day did different do does don't down each earth eat end enough even every example eye face family far father feet few find first follow food for form found four from get girl give go good got great group grow had hand hard has have he head hear help her here high him his home house how idea if important in Indian into is it its it's just keep kind know land large last later learn leave left let letter life light like line list little live long look made make man many may me mean men might mile miss more most mother mountain move much must my name near need never new next night no not now number of off often oil old on once one only open or other our out over own page paper part people picture place plant play point put question quick quickly quite read really right river run said same saw say school sea second see seem sentence set she should show side small so some something sometimes song soon sound spell start state still stop story study such take talk tell than that the their them then there these they thing think this those thought three through time to together too took tree try turn two under until up us use very walk want was watch water way we well went were what when where which while white who why will with without word work world would write year you young your";
+let wordList = quote.split(" ");
+console.log(wordList);
+
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+let sentIn = [];
+
+shuffle(wordList);
+let newWordList = wordList.toString();
+newWordList = newWordList.replace(/,/g, ' ')
+const inputField = document.getElementById("input-field");
+const accDisp = document.getElementById("Accuracy");
+const wpmDisp = document.getElementById("WPM");
+
+document.getElementById("tt").innerHTML = newWordList;
+inputField.value = "";
+inputField.focus();
+
+inputField.addEventListener("keypress", startFunc); 
+
+let i = 0
+let cor = 0;
+let incr = 0;
+let cpm = 0;
+function checkIfaccurate(){
+    if(inputField.value == wordList[i]){
+        cor += 1;
+        cpm += inputField.value.length
+    } else {
+        incr += 1;
+
+    }
+    i += 1;
+    sentIn.push(inputField.value);
+    inputField.value = "";
+    inputField.value.replace(/\s+/g, '');
+}
+
+function compute(){
+    let acc = Math.round(100-((incr/cor) * 100));
+    let wpm = cpm/5;
+    accDisp.innerHTML = acc + "%";
+    wpmDisp.innerHTML = wpm;
+
+}
+function checkCorrect(char){
+    if(char.which == 32){
+        checkIfaccurate();
+        char.preventDefault();
+        return false;
+    }
+}
+function startFunc() {
+    inputField.removeEventListener("keypress", startFunc);
+    console.log("before");
+    inputField.addEventListener("keypress", checkCorrect);
+    setTimeout(endFunc, 60000);
+}
+
+function endFunc() {
+    console.log("60 seconds are over");
+    compute();
+}
