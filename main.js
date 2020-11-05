eb66b9dec1c61a246c7e606121a665e203c02473
Omar Ansari <StreetFighterLan123@gmail.com>
2020-11-05 09:52:01 -0600

    Noto Sans

--- main.js
+++ main.js
@@ -56,7 +56,8 @@ console.log(nodes)
 function checkIfaccurate(){
     if(inputField.value == wordList[i]){
         // Correct color
-        nodes[i].style.color = "#506531";
+        //nodes[i].style.color = "#506531";
+        nodes[i].style.color = '#33b8b8';
         cor += 1;
         cpm += inputField.value.length
         keysPressed += 1;
